3af2fb15af8f3103038cafe0db52efaf7f938446
Thejus <git@tigerraw22.anonaddy.com>
2022-05-07 18:36:46 +0530

    Done

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,27 @@
 var text = document.getElementsByClassName("text"),
     wholepage = document.getElementsByClassName("Wholepage"),
-    bar=document.getElementById("buger");
+    bar=document.getElementById("buger"),
+    cross=document.getElementById("cross");
 
 function open(){
-    text.style.visibility = "visible";
-    console.log("s")
-    // wholepage
+    for(var i = 0; i < text.length; i++)
+    {
+        text.item(i).style.visibility = "visible";
+    }
+    cross.style.visibility = "visible";
+    bar.style.visibility = "hidden";
+    wholepage.item(0).style.gridTemplateColumns = "300px auto";
+}
+
+function close(){
+    for(var i = 0; i < text.length; i++)
+    {
+        text.item(i).style.visibility = "hidden";
+    }
+    cross.style.visibility = "hidden";
+    bar.style.visibility = "visible";
+    wholepage.item(0).style.gridTemplateColumns = "100px auto";
 }
 
 bar.onclick = open;
+cross.onclick = close;
