1669c317ff7c6ed1575bd89e981173c1595eeb3a
Thejus <git@tigerraw22.anonaddy.com>
2022-05-07 15:15:12 +0530

    just the opening of navbar left

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+var text = document.getElementsByClassName("text"),
+    wholepage = document.getElementsByClassName("Wholepage"),
+    bar=document.getElementById("buger");
+
+function open(){
+    text.style.visibility = "visible";
+    console.log("s")
+    // wholepage
+}
+
+bar.onclick = open;
